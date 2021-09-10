496dc899659fc9fdc2df08916aaaa5e69ed09def
Branden Wheeler <branden.wheeler17@gmail.com>
2021-09-10 17:07:16 -0400

    Establish connection to DB and create projects page

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require("mongoose");
 const express = require("express");
+
 const app = express();
 app.use(express.static("public"));
+app.use(express.json());
 app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/projectsDB");
+const projectSchema = mongoose.Schema({
+  title: String,
+  content: String,
+  img: {
+    type: Buffer,
+    contentType: String
+  }
+})
+const Project = mongoose.model("Project", projectSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -9,6 +23,16 @@ app.get("/", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.render("contact");
+});
+
+app.get("/projects", function(req, res) {
+  Project.find(function(err, projects) {
+    if(!err) {
+      res.render("projects", {projects: projects});
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 app.listen(3000, function () {
