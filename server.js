8f679692f5ec2367d48d053cbbe7d4569f38d590
Branden Wheeler <branden.wheeler17@gmail.com>
2021-09-11 21:46:31 -0400

    Add post route for adding projects to db

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 app.use(express.static("public"));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/projectsDB");
 const projectSchema = mongoose.Schema({
   title: String,
   content: String,
-  img: {
-    type: Buffer,
-    contentType: String
-  }
-})
+  imageName: String
+});
 const Project = mongoose.model("Project", projectSchema);
 
 app.get("/", function (req, res) {
@@ -34,6 +32,21 @@ app.get("/projects", function(req, res) {
     }
   });
 });
+
+app.get("/projects-post", function(req, res) {
+  res.render("projects-post")
+})
+
+app.post("/projects-post", function(req, res) {
+  console.log(req.body.title);
+  const newProject = new Project({
+    title: req.body.title,
+    content: req.body.content,
+    imageName: req.body.imgName
+  });
+  newProject.save();
+  res.redirect("/projects");
+})
 
 app.listen(3000, function () {
   console.log("Server running on port 3000");
